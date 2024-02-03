d5982587a60845a47a1ad4b0aac529a7ac378ea6
kevalrajpalknight <kevalprogram100@gmail.com>
2024-02-04 00:38:29 +0530

    fix the setup code

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from redis import Redis
+from rq import Queue
 
 import users.models as user_models
+from config.constants import REDIS_HOST, REDIS_PORT
 from config.database.config import engine
 from users import route as user_route
 
@@ -17,10 +20,7 @@ app = FastAPI(
     license_info=None,
 )
 
+redis_conn = Redis(host=REDIS_HOST, port=REDIS_PORT)
+task_queue = Queue("task_queue", connection=redis_conn)
+
 app.include_router(user_route.router, prefix="/users", tags=["users"])
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
