82deab3a44e1fe664936302855ff8ccf3218eac8
sarveshsta <sarvesh@seizetheads.com>
2024-03-07 10:28:17 +0530

    reverted changes

--- trades/strategy/__init__.py
+++ trades/strategy/__init__.py
@@ -68,7 +68,7 @@ class InstrumentReaderInterface:
 
 
 class OpenApiInstrumentReader(InstrumentReaderInterface):
-    def __init__(self, url: str, tokens: List[str] = None):
+    def __init__(self, url: str, tokens: List[str]):
         self.url = url
         self.tokens = tokens or []
 
@@ -121,7 +121,6 @@ class IndicatorInterface:
     def check_indicators(self, data: pd.DataFrame) -> List[str]:
         raise NotImplementedError("Subclasses must implement check_indicators()")
 
-
 class MaxMinOfLastTwo(IndicatorInterface):
     DOUBLE_HIGH_MULTIPLIER = 1.012
     DOUBLE_LOW_MULTIPLIER = 0.988
@@ -131,87 +130,126 @@ class MaxMinOfLastTwo(IndicatorInterface):
     to_sell = False
     waiting_for_sell = False
     waiting_for_buy = False
-    bought_price = 0
+    price = 0
     sold_price = 0
     cum_profit = 0
     trades_count = 0
     current_index = 0
-    LAST_OHLC = 4
-    SECOND_LAST_OHLC = 5
+    # LAST_OHLC = 4
+    # SECOND_LAST_OHLC = 5
     CURRENT_OHLC = 5
 
     def check_indicators(self, data: pd.DataFrame, index: int = 0) -> tuple[Signal, float]:
-        print("Index: ", index)
-        print("Data: ", data.iloc[self.CURRENT_OHLC])
-        if index == 2:
-            previous_max_high = max(data["High"].iloc[self.LAST_OHLC], data["High"].iloc[self.SECOND_LAST_OHLC])
-            new_high = previous_max_high * self.DOUBLE_HIGH_MULTIPLIER
+        current_open = data["Open"].iloc[self.CURRENT_OHLC]
+        last_third_open = data["Open"].iloc[self.CURRENT_OHLC - 2]
+        print("LATEST/LAST CANDLE OPEN", f"{current_open} ** {last_third_open}")
 
-            self.to_buy = False
-            self.to_sell = False
-            self.waiting_for_buy = True
-            self.bought_price = new_high
-            return Signal.WAITING_TO_BUY, self.bought_price
-        if index > 2:
-            if not self.to_buy and not self.to_sell and self.waiting_for_buy:
-                if data["High"].iloc[self.CURRENT_OHLC] > self.bought_price:
-                    self.to_buy = False
-                    self.to_sell = True
-                    self.waiting_for_buy = False
-                    return Signal.SELL, self.bought_price
-
-                if self.waiting_for_buy:
-                    previous_max_high = (
-                        max(data["High"].iloc[self.LAST_OHLC], data["High"].iloc[self.SECOND_LAST_OHLC])
-                        * self.DOUBLE_HIGH_MULTIPLIER
-                    )
-                    previous_high = (data["High"].iloc[self.LAST_OHLC]) * self.SINGLE_HIGH_MULTIPLIER
-                    new_high = min(previous_max_high, previous_high)
-                    if new_high != self.bought_price:
-                        self.bought_price = new_high
-                        return Signal.WAITING_TO_BUY, self.bought_price
-
-            elif not self.to_buy and self.to_sell:
-                last_two_low = []
-                last_two_low.append(data["Low"].iloc[self.SECOND_LAST_OHLC])
-                last_two_low.append(data["Low"].iloc[self.LAST_OHLC])
-                min_last_two_low = self.DOUBLE_LOW_MULTIPLIER * min(last_two_low)
-                previous_low = (data["Low"].iloc[self.LAST_OHLC]) * self.SINGLE_LOW_MULTIPLIER
-                new_low = max(min_last_two_low, previous_low)
-                self.sold_price = new_low
-                self.to_buy = False
-                self.to_sell = False
+        # condition to buy
+        if not self.to_buy:
+            if current_open >= (last_third_open + (last_third_open * 0.01)):
+                self.to_buy = True
                 self.waiting_for_sell = True
-                return Signal.WAITING_TO_SELL, self.sold_price
 
-            elif not self.to_buy and not self.to_sell and self.waiting_for_sell:
-                if data["Low"].iloc[self.CURRENT_OHLC] < self.sold_price:
-                    self.trades_count += 1
-                    self.to_buy = True
-                    self.to_sell = False
-                    self.waiting_for_sell = False
-                    return Signal.BUY, self.sold_price
-                if self.waiting_for_sell:
-                    last_two_low = []
-                    last_two_low.append(data["Low"].iloc[self.SECOND_LAST_OHLC])
-                    last_two_low.append(data["Low"].iloc[self.LAST_OHLC])
-                    min_last_two_low = self.DOUBLE_LOW_MULTIPLIER * min(last_two_low)
-                    previous_low = (data["Low"].iloc[self.LAST_OHLC]) * self.SINGLE_LOW_MULTIPLIER
-                    new_low = max(min_last_two_low, previous_low)
-                    if new_low != self.sold_price:
-                        self.sold_price = new_low
-                        return Signal.WAITING_TO_SELL, self.sold_price
-
-            elif self.to_buy and not self.to_sell:
-                previous_max_high = max(data["High"].iloc[self.LAST_OHLC], data["High"].iloc[self.SECOND_LAST_OHLC])
-                new_high = previous_max_high * self.DOUBLE_HIGH_MULTIPLIER
-                self.to_buy = False
                 self.to_sell = False
+                self.waiting_for_buy = False
+                self.price = current_open
+                return Signal.BUY, self.price
+            return Signal.WAITING_TO_BUY, self.price
+        
+        # condition to sell 
+        elif self.to_buy and not self.to_sell and self.waiting_for_sell:
+            if (data["High"].iloc[self.CURRENT_OHLC - 2] >= self.price * 1.10) or \
+                    (data["Low"].iloc[self.CURRENT_OHLC - 2] <= self.price * 0.95):
+                self.to_sell = True
                 self.waiting_for_buy = True
-                self.bought_price = new_high
-                return Signal.WAITING_TO_BUY, self.bought_price
 
-        return None, None
+                self.to_buy = False
+                self.waiting_for_sell = False
+
+                return Signal.SELL, self.price
+            return Signal.WAITING_TO_SELL, self.price
+
+        elif not self.to_sell and self.to_buy and not self.waiting_for_buy:
+            self.waiting_for_sell = True
+            return Signal.WAITING_TO_SELL, self.price
+        
+        else:
+            self.waiting_for_buy = True
+            return Signal.WAITING_TO_BUY, self.price
+            
+    # def check_indicators(self, data: pd.DataFrame, index: int = 0) -> tuple[Signal, float]:
+    #     print("Index: ", index)
+    #     print("Data: ", data.iloc[self.CURRENT_OHLC])
+    #     if index == 2:
+    #         previous_max_high = max(data["High"].iloc[self.LAST_OHLC], data["High"].iloc[self.SECOND_LAST_OHLC])
+    #         new_high = previous_max_high * self.DOUBLE_HIGH_MULTIPLIER
+
+    #         self.to_buy = False
+    #         self.to_sell = False
+    #         self.waiting_for_buy = True
+    #         self.bought_price = new_high
+    #         return Signal.WAITING_TO_BUY, self.bought_price
+    #     if index > 2:
+    #         if not self.to_buy and not self.to_sell and self.waiting_for_buy:
+    #             if data["High"].iloc[self.CURRENT_OHLC] > self.bought_price:
+    #                 self.to_buy = False
+    #                 self.to_sell = True
+    #                 self.waiting_for_buy = False
+    #                 return Signal.SELL, self.bought_price
+
+    #             if self.waiting_for_buy:
+    #                 previous_max_high = (
+    #                     max(data["High"].iloc[self.LAST_OHLC], data["High"].iloc[self.SECOND_LAST_OHLC])
+    #                     * self.DOUBLE_HIGH_MULTIPLIER
+    #                 )
+    #                 previous_high = (data["High"].iloc[self.LAST_OHLC]) * self.SINGLE_HIGH_MULTIPLIER
+    #                 new_high = min(previous_max_high, previous_high)
+    #                 if new_high != self.bought_price:
+    #                     self.bought_price = new_high
+    #                     return Signal.WAITING_TO_BUY, self.bought_price
+
+    #         elif not self.to_buy and self.to_sell:
+    #             last_two_low = []
+    #             last_two_low.append(data["Low"].iloc[self.SECOND_LAST_OHLC])
+    #             last_two_low.append(data["Low"].iloc[self.LAST_OHLC])
+    #             min_last_two_low = self.DOUBLE_LOW_MULTIPLIER * min(last_two_low)
+    #             previous_low = (data["Low"].iloc[self.LAST_OHLC]) * self.SINGLE_LOW_MULTIPLIER
+    #             new_low = max(min_last_two_low, previous_low)
+    #             self.sold_price = new_low
+    #             self.to_buy = False
+    #             self.to_sell = False
+    #             self.waiting_for_sell = True
+    #             return Signal.WAITING_TO_SELL, self.sold_price
+
+    #         elif not self.to_buy and not self.to_sell and self.waiting_for_sell:
+    #             if data["Low"].iloc[self.CURRENT_OHLC] < self.sold_price:
+    #                 self.trades_count += 1
+    #                 self.to_buy = True
+    #                 self.to_sell = False
+    #                 self.waiting_for_sell = False
+    #                 return Signal.BUY, self.sold_price
+    #             if self.waiting_for_sell:
+    #                 last_two_low = []
+    #                 last_two_low.append(data["Low"].iloc[self.SECOND_LAST_OHLC])
+    #                 last_two_low.append(data["Low"].iloc[self.LAST_OHLC])
+    #                 min_last_two_low = self.DOUBLE_LOW_MULTIPLIER * min(last_two_low)
+    #                 previous_low = (data["Low"].iloc[self.LAST_OHLC]) * self.SINGLE_LOW_MULTIPLIER
+    #                 new_low = max(min_last_two_low, previous_low)
+    #                 if new_low != self.sold_price:
+    #                     self.sold_price = new_low
+    #                     return Signal.WAITING_TO_SELL, self.sold_price
+
+    #         elif self.to_buy and not self.to_sell:
+    #             previous_max_high = max(data["High"].iloc[self.LAST_OHLC], data["High"].iloc[self.SECOND_LAST_OHLC])
+    #             new_high = previous_max_high * self.DOUBLE_HIGH_MULTIPLIER
+    #             self.to_buy = False
+    #             self.to_sell = False
+    #             self.waiting_for_buy = True
+    #             self.bought_price = new_high
+    #             return Signal.WAITING_TO_BUY, self.bought_price
+
+    #     return None, None
+
 
 
 class BaseStrategy:
@@ -238,6 +276,8 @@ class BaseStrategy:
             print("_" * 10)
             print("Signal: ", signal)
             print("Price: ", price)
+            print("Data: ", token_data.iloc[index])
+            print("Time: ", token_data.iloc[index]["timestamp"])
             print("_" * 10)
 
     def start_strategy(self):
@@ -261,13 +301,19 @@ CLIENT_CODE = None
 PASSWORD = None
 TOKEN_CODE = None
 
+API_KEY = "T4MHVpXH"
+CLIENT_CODE = "J263557"
+PASSWORD = "7753"
+TOKEN_CODE = "3MYXRWJIJ2CZT6Y5PD2EU5RNNQ"
+
 api_key = API_KEY
 token_code = TOKEN_CODE
 client_code = CLIENT_CODE
 password = PASSWORD
 
 smart = SmartConnect(api_key=api_key)
-data = smart.generateSession(
+data = \
+    smart.generateSession(
     clientCode=client_code,
     password=password,
     totp=pyotp.TOTP(token_code).now(),
