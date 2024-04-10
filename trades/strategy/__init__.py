4c3c5683cb29e17276e9c101a8957316ab4301f8
sarveshsta <sarvesh@seizetheads.com>
2024-04-10 11:56:28 +0530

    expert strategy

--- trades/strategy/__init__.py
+++ trades/strategy/__init__.py
@@ -18,6 +18,7 @@ from SmartApi import SmartConnect
 class CandleDuration(Enum):
     ONE_MINUTE = "ONE_MINUTE"
     THREE_MINUTE = "THREE_MINUTE"
+    FOUR_MINUTE = "FOUR_MINUTE"
     FIVE_MINUTE = "FIVE_MINUTE"
     TEN_MINUTE = "TEN_MINUTE"
 
@@ -76,12 +77,13 @@ class OpenApiInstrumentReader(InstrumentReaderInterface):
         try:
             # Robust error handling to ensure proper data reading
             response = requests.get(self.url)
-            response.raise_for_status()  # Raise an exception for HTTP errors
+            # print("Response", response)
+            response.raise_for_status()
             data = response.json()
             return [
                 Instrument(**item)
                 for item in data
-                if item["exch_seg"] == "NFO" and item["symbol"] in self.tokens  # Filter for NFO instruments only
+                if item["exch_seg"] == "NFO" and item["symbol"] in self.tokens
             ]
         except (URLError, JSONDecodeError) as e:
             print(f"Error reading instruments from {self.url}: {e}")
@@ -113,7 +115,7 @@ class SmartApiDataProvider(DataProviderInterface):
 
         columns = ["timestamp", "Open", "High", "Low", "Close", "Volume"]
         data = pd.DataFrame(res_json["data"], columns=columns)
-        print("Data Provided: ", data)
+        # print("Data Provided: ", data)
         return data
 
 
@@ -138,35 +140,42 @@ class MaxMinOfLastTwo(IndicatorInterface):
     LAST_OHLC = 4
     SECOND_LAST_OHLC = 5
     CURRENT_OHLC = 5
+    trade_details = {"done": False, "index": None}
 
-
-    def check_indicators(self, data: pd.DataFrame, index: int = 0) -> tuple[Signal, float]:
-        current_open = data["Open"].iloc[self.CURRENT_OHLC]
-        last_third_open = data["Open"].iloc[self.CURRENT_OHLC - 2]
-        print("LATEST/LAST CANDLE OPEN", f"{current_open} ** {last_third_open}")
-
-        # condition to buy
+    def check_indicators(self, data: pd.DataFrame, token:pd.DataFrame,  index: int = 0) -> tuple[Signal, float]:
+        current_high = data["High"].iloc[-1]
+        last_second_high = data["Open"].iloc[-2]
+        last_third_high = data["Open"].iloc[-3]
+        token = str(token).split(':')[-1]
+        print("DATA FRAME", data)
         if not self.to_buy:
-            if current_open >= (last_third_open + (last_third_open * 0.01)):
+            if current_high >= (last_second_high + (last_second_high * 0.01)) or current_high >= (last_third_high + (last_third_high * 0.01)):
+                print("CH/CH1/CH2", current_high, last_second_high, last_third_high)
                 self.to_buy = True
                 self.waiting_for_sell = True
 
                 self.to_sell = False
                 self.waiting_for_buy = False
-                self.price = current_open
+                self.price = current_high
+                self.trade_details['done'] = True
+                self.trade_details['index'] = token
+                print("TRADE BOUGHT", self.trade_details)
+                print("Bought Price", self.price,  )
                 return Signal.BUY, self.price
             return Signal.WAITING_TO_BUY, self.price
         
         # condition to sell 
-        elif self.to_buy and not self.to_sell and self.waiting_for_sell:
-            if (data["High"].iloc[self.CURRENT_OHLC - 2] >= self.price * 1.10) or \
-                    (data["Low"].iloc[self.CURRENT_OHLC - 2] <= self.price * 0.95):
+        elif self.to_buy and not self.to_sell and self.waiting_for_sell \
+            and self.trade_details['index'] == token:
+            if (data["High"].iloc[-1] >= self.price * 1.10) or \
+                    (data["Low"].iloc[-1] <= self.price * 0.95):
+                
                 self.to_sell = True
                 self.waiting_for_buy = True
 
                 self.to_buy = False
                 self.waiting_for_sell = False
-
+                print("TRADE SOLD", self.trade_details)
                 return Signal.SELL, self.price
             return Signal.WAITING_TO_SELL, self.price
 
@@ -176,6 +185,8 @@ class MaxMinOfLastTwo(IndicatorInterface):
         
         else:
             self.waiting_for_buy = True
+            self.trade_details['done'] = False
+            self.trade_details['index'] = None
             return Signal.WAITING_TO_BUY, self.price
             
   
@@ -199,11 +210,12 @@ class BaseStrategy:
         nfo_tokens = [Token(instrument.exch_seg, instrument.token) for instrument in self.instruments]
         data = {token: self.data_provider.fetch_candle_data(token) for token in nfo_tokens}
         for token, token_data in data.items():
-            signal, price = self.indicator.check_indicators(token_data, index)
+            signal, price = self.indicator.check_indicators(token_data, token,  index)
             print("_" * 10)
             print("Signal: ", signal)
             print("Price: ", price)
             print("Data: ", token_data.iloc[index])
+            print("Token: ", token)
             print("Time: ", token_data.iloc[index]["timestamp"])
             print("_" * 10)
 
@@ -241,9 +253,9 @@ data = \
     totp=pyotp.TOTP(token_code).now()
 )
 auth_token = data["data"]["jwtToken"]
-feed_token = smart.getfeedToken()
+feed_token = smart.getfeedToken() 
 
-instrument_reader = OpenApiInstrumentReader(NFO_DATA_URL, ["BANKNIFTY03APR2444400PE"])
+instrument_reader = OpenApiInstrumentReader(NFO_DATA_URL, ["FINNIFTY09APR2421500PE", "BANKNIFTY10APR2448200CE"])
 smart_api_provider = SmartApiDataProvider(smart)
 max_transactions_indicator = MaxMinOfLastTwo()
 strategy = BaseStrategy(instrument_reader, smart_api_provider, max_transactions_indicator)
