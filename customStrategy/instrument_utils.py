5df1f0708b40ab148ef27d67b6e78dacf92aa1a3
Akshay <akshaykumravat0701@gmail.com>
2025-09-12 17:48:29 +0530

    added more logs

--- customStrategy/instrument_utils.py
+++ customStrategy/instrument_utils.py
@@ -4,9 +4,12 @@ from typing import List, Dict
 
 def get_instruments_from_openapi(url: str, tokens: List[str]) -> List[Dict]:
     try:
+        print(url, "url")
         response = requests.get(url)
+        print(response, "response")
         response.raise_for_status()
         data = response.json()
+        print(data, "data")
         print("Tokens to match:", tokens)
         filtered = [
             item for item in data
